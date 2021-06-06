afba6c54d937253fb64b06fc5209c4d02fe48410
Oleksandr <sasha.liruk@gmail.com>
2021-06-07 00:00:07 +0300

    S_2

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+
+function Navbar() {
+    <nav className="navbar navbar-dark navbar-expand-lg bg-primery">
+        <div className="navbar-brand">
+            Note App
+        </div>
+
+    </nav>
+}
+
+export default Navbar
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+function Home() {
+    return (
+       <Fragment>
+         <h1>
+             Home page
+         </h1>
+       </Fragment>
+    )
+}
+
+export default Home
